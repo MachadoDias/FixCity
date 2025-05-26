89e304ecf77bda568f25ff16d28eee0a3b44887f
MachadoDias <gabrielgabriel181007@gmail.com>
2025-05-26 19:00:23 -0300

    Arquivos de treinamento

--- bot/index.js
+++ bot/index.js
@@ -1,6 +1,17 @@
 const venom = require('venom-bot');
 venom 
     .create({
-        session: "session-name"
-    })
-    .then(client => console.log)
+  session: "session-name",
+  puppeteerOptions: {
+    headless: 'new',
+    executablePath: '/opt/google/chrome/chrome'
+  }
+})
+    .then(client => start(client))
+    .catch(error => console.error(error))
+
+function start(client){
+    client.sendText("5535997286802@c.us", "ejdjeifjiemjdwimj")
+    .then(result => console.log(result))
+    .catch(error => console.error(error))
+}
